f987898250f276ec37199feda6740e5083023e2d
Dan Harcsztark <85728+dnstock@users.noreply.github.com>
2025-02-24 12:34:02 -0500

    Update brew: Add new upgrade alias

--- brew.zsh
+++ brew.zsh
@@ -6,7 +6,7 @@ export PATH="/usr/local/sbin:$PATH"
 
 _add_alias brewup "brew update; brew cleanup; echo '==> Outdated brews:'; brew outdated --verbose; echo '==> Outdated casks:'; brew outdated --cask --verbose" "Update homebrew and list outdated packages"
 _add_alias brewupd "brewup; echo '==> Doctor Brew:'; brew doctor" "Update homebrew, list outdated packages, and run brew doctor"
-# fix cask: brew uninstall --force brew-cask; brew update
+_add_alias brewupg "echo '==> Upgrading brews:'; brew upgrade; echo '----------------'; brewup" "Updating homebrew packages list"
 
 _add_alias brewserv "brew services" "Manage homebrew services"
 
